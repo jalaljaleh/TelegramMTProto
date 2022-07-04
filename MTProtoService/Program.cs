dd6623d0936a63d3d1aed73906dce80b8a98f6f6
Jalal Jaleh <JalalJaleh@gmail.com>
2022-07-04 16:54:49 +0430

    update

---
+++ MTProtoService/Program.cs
@@ -0,0 +1,13 @@
+﻿using TelegramMTProto;
+
+Console.WriteLine("Starting ..");
+
+var secret = Environment.GetEnvironmentVariable("secret") ?? throw new Exception("Secret can't be null.");
+var port = int.Parse(Environment.GetEnvironmentVariable("port") ?? throw new Exception("Port can't be null."));
+var ip = Environment.GetEnvironmentVariable("ip") ?? "default";
+
+TelegramMTProtoServer protoServer = new TelegramMTProtoServer(secret, port, ip);
+protoServer.Start();
+
+Console.WriteLine("Started");
+Console.ReadLine();
---
+++ Procfile
@@ -0,0 +1,2 @@
+web: cd $HOME/heroku_output && ./MTProtoService --server.urls http://+:$PORT ${CORE_ENVIRONMENT}
+worker cd $HOME/heroku_output && ./MTProtoService
